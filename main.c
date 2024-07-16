db308554df0a5ea6d1f0da72a3f51160308897e1
Alexander Haberstroh <86650791+A-Haberstroh@users.noreply.github.com>
2024-07-16 13:30:08 +0200

    Insert issues in SonarCloud

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+typedef enum {APPLE, GRAPE, KIWI} fruit;
+
+void example(fruit f, int i) {
+  switch (f) {  // Noncompliant; no case for KIWI
+    case APPLE:
+      //...
+    case GRAPE:
+      //...
+    case 3: // Noncompliant; case value not in enum
+      // ...
+  }
+
 int main() {
+
+  switch (i) { // Noncompliant; no default
+    case 0:
+    // ...
+    case 1:
+    // ...
+    }
+  }
+    
     printf("Hello, World!\n");
     return 0;
 }
